0bd450c727cf0a5c09338484950bcbb65c18a2e7
fouadmo1223 <fm0850020@gmail.com>
2025-05-13 21:20:48 +0300

    Remove israel

--- src/utils/countryApi.js
+++ src/utils/countryApi.js
@@ -1,8 +1,12 @@
 
 export const fetchCountryData = async (countryName) => {
-    if (!countryName.trim()) return null;
     
+    if (!countryName.trim()) return null;
+    if(countryName.trim().toLowerCase() == 'israel' ) {
+        countryName = 'Palestine';
+    }
     try {
+        
       const response = await fetch(`https://restcountries.com/v3.1/name/${countryName.trim()}?fullText=true`);
       
       if (!response.ok) {
